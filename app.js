e9441b0e8466755e5084c00ae7dd88a492e1ee7e
MDMinhazulIslamKhan <113751180+MDMinhazulIslamKhan@users.noreply.github.com>
2023-06-15 01:15:31 +0600

    fix: edid  package.json

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+
+import { CommonRouters } from "./routes/v1/common/index.js";
+import { PublicRouters } from "./routes/v1/public/index.js";
+import { isUser } from "./middleware/public/index.js";
+
+const app = express();
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Welcome route
+app.get("/", (req, res) => {
+  return res.status(200).send("Welcome in Test Task application!");
+});
+
+// application common routes
+app.use("/api/v1/common", CommonRouters);
+
+// application public routes
+app.use("/api/v1/public", isUser, PublicRouters);
+
+//No route
+app.all("*", (req, res) => {
+  const route = req.params;
+  return res.status(404).send(`No Route found in "${route[0]}" !`);
+});
+
+export default app;
--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-import express from "express";
-import cors from "cors";
+import mongoose from "mongoose";
+import app from "./app.js";
+import config from "./config/index.js";
 
-import { CommonRouters } from "./routes/v1/common/index.js";
-import { PublicRouters } from "./routes/v1/public/index.js";
-import { isUser } from "./middleware/public/index.js";
+const port = process.env.PORT || 5000;
 
-const app = express();
-app.use(cors());
+async function main() {
+  try {
+    await mongoose.connect(`${config.database_url}/${config.database_name}`);
+    console.log(`Database is connected on port ${config.port}`);
 
-// parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    app.listen(port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(`Failed to connect database, ${error}`);
+  }
+}
 
-//Welcome route
-app.get("/", (req, res) => {
-  return res.status(200).send("Welcome in Test Task application!");
-});
-
-// application common routes
-app.use("/api/v1/common", CommonRouters);
-
-// application public routes
-app.use("/api/v1/public", isUser, PublicRouters);
-
-//No route
-app.all("*", (req, res) => {
-  const route = req.params;
-  return res.status(404).send(`No Route found in "${route[0]}" !`);
-});
-
-export default app;
+main();
