cc3db81a3428696077e914f6fa48e00fc8f91193
MDMinhazulIslamKhan <113751180+MDMinhazulIslamKhan@users.noreply.github.com>
2023-06-15 00:52:32 +0600

    fix: edit port

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 import app from "./index.js";
 import config from "./config/index.js";
 
+const port = process.env.PORT || 5000;
+
 async function main() {
   try {
     await mongoose.connect(`${config.database_url}/${config.database_name}`);
     console.log(`Database is connected on port ${config.port}`);
 
-    app.listen(config.port, () => {
+    app.listen(port, () => {
       console.log(`Application listening on port ${config.port}`);
     });
   } catch (error) {
